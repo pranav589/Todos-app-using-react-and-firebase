f1bb8fc9b9c91223df1958c91ca7ddb63b282e64
pranav589 <pranav.bakale@gmail.com>
2020-08-17 17:46:23 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import "./style.css";
 export default function App() {
 
   const [todos,setTodos]=useState(['tea','coffe'])
+
+  const [input,setInput]=useState('')
+  //console.log(input)
+
+  const addTodo=(event)=>{
+     //console.log('clicked')
+     event.preventDefault()
+     setTodos([...todos,input])
+     setInput('')
+  }
   return (
     <div>
-      <input/>
-      <button>Add todo</button>
+    <form>
+      <input value={input} onChange={event=>setInput(event.target.value)}/>
+      <button type='submit' onClick={addTodo}>Add todo</button>
+    </form>
     <ul>
       {todos.map(todo=>(
         <li>{todo}</li>
